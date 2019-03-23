60363edf56a122b686cf977e6e154fc55807d14f
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-03-23 19:00:13 +0300

    feat: add function check which checks open-closed brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !==0) return false;
+  let arr = [];
+  for ( let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][1] && bracketsConfig[j][0] == arr[arr.length - 1]) arr.pop();
+      else if (str[i] == bracketsConfig[j][0]) arr.push(str[i]);
+    }
+  } return arr.length == 0;
 }
+
